dd48d3dcec08d096e63800625b1c468fb0d0d68d
Shady GOMAA <shadyahmed9719@gmail.com>
2023-01-05 04:58:31 +0200

    create Java project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+    public void generateNID(){
+        // store random year
+        int year=getRandomNumberUsingNextInt(0,99);
+        if(year<)
+    }
+}
